932f60eb1af4b82340f1f6ce5192f287c9bef73d
wronnyhuang <wrhuang@mit.edu>
2018-09-05 13:58:02 -0400

    wip

--- tboard.sh
+++ tboard.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+pkill -f 'tensorboard --port=6006'
 logdir=${1:-./}
+nohup tensorboard --port=6006 --logdir=$logdir &
 echo tensorboarding directory $logdir
-tensorboard --logdir $logdir
+if [ $2 = "n" ]; then
+	pkill -f ngrok
+	nohup ngrok http 6006 &
+	echo tunneling with ngrok
+	sleep 1
+	curl -s http://localhost:4040/api/tunnels | python3 -c \
+    "import sys, json; print(json.load(sys.stdin)['tunnels'][0]['public_url'])"
+fi
