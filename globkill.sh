36d67d4052f42fe2cbea7a0800f6ebf0938a7773
Ronny <wrhuang@mit.edu>
2018-11-10 17:22:01 -0500

    clean up and organize

--- globkill.sh
+++ globkill.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-userhost=( ronny@tricky.cs.umd.edu ronny@tomg3264.cs.umd.edu rhuang@max.cs.umd.edu )
+userhost=( ronny@tricky.cs.umd.edu ronny@tomg3264.cs.umd.edu rhuang@max.cs.umd.edu ronny@dsvm6usdum456zy76.eastus.cloudapp.azure.com)
 for uh in "${userhost[@]}"
 do
 	ssh -t $uh "rek $1"
