9c308c9e7713fcbff3933378b4bddd90a47fe418
Ronny <wrhuang@mit.edu>
2018-11-22 23:41:49 -0500

    updated hpsopt filein mnay ways

--- build_dockerfile.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ssh -t ronny@tomg3264.cs.umd.edu "cd \$HOME/bin && git pull && cd dockerfile && docker build -t wrhuang/add . && docker push wrhuang/add"
--- gitclone.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-git clone https://github.com/wronnyhuang/$1
