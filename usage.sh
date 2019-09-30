0b27d824e34e92a13af222363a637772f8200a6c
wrhuang <Ronny.Huang@ey.com>
2019-09-30 13:26:58 -0400

    wip

--- usage.sh
+++ usage.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 while true ; do
-    echo -n `date`,\  >> usage.log
-    nvidia-smi | grep -oh "[0-9]*\%" | while read perc ; do echo -n $perc,\  >> usage.log ; done
+    echo -n `date`,\t >> usage.log
+    nvidia-smi | grep -oh "[0-9]*\%" | while read perc ; do echo -n $perc,\t >> usage.log ; done
     echo "" >> usage.log
     sleep 1
 done
