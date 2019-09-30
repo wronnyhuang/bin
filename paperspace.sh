ec127977ab258065fd759998fa2f1f96ffcfd97b
wrhuang <Ronny.Huang@ey.com>
2019-09-30 13:15:26 -0400

    usage

--- paperspace.sh
+++ paperspace.sh
@@ -2,5 +2,6 @@
 userhost=( paperspace@74.82.31.21 paperspace@65.49.81.37 paperspace@184.105.87.14 paperspace@74.82.31.120 )
 for uh in "${userhost[@]}"
 do
-    ssh -t $uh "git clone https://github.com/wronnyhuang/metapoison"
+    ssh -t $uh "$1"
+#    ssh-copy-id $uh
 done
---
+++ usage.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+while true ; do
+    smi | grep -oh "[0-9]*\%" | while read perc ; do echo -n $perc,\  >> usage.log ; done && printf "\n"
+    sleep 1
+done
