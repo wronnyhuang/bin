36091c1d6d6f5e4555c5c5283ac4559e931fcc5e
wrhuang <Ronny.Huang@ey.com>
2020-03-17 15:46:40 -0400

    ok

--- init_anaconda_cml.sh
+++ init_anaconda_cml.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
+user=`whoami`
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/cmlscratch/`whoami`/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$('/cmlscratch/$user/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/cmlscratch/`whoami`/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/cmlscratch/`whoami`/anaconda3/etc/profile.d/conda.sh"
+    if [ -f "/cmlscratch/$user/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/cmlscratch/$user/anaconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/cmlscratch/`whoami`/anaconda3/bin:$PATH"
+        export PATH="/cmlscratch/$user/anaconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
 # <<< conda initialize <<<
 
-export PATH="/cmlscratch/`whoami`/anaconda3/bin:$PATH"
+export PATH="/cmlscratch/$user/anaconda3/bin:$PATH"
 conda activate tf114
-cd /cmlscratch/`whoami`/metapoison
+cd /cmlscratch/$user/metapoison
